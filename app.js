d55fb3ac467bbff444b39b6bb5133d960c04090d
Silavsale <silavsale@gmail.com>
2019-04-04 20:38:50 -0400

    connect simplePeer

--- app.js
+++ app.js
@@ -1,10 +1,54 @@
-document.querySelector('#start').addEventListener('click', function (e) {
+navigator.getUserMedia = navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia;
+
+function bindEvents(p) {
+
+    p.on('error', function (err) {
+        console.log('error', err)
+    });
+
+    p.on('signal', function (data) {
+        document.querySelector("#offer").textContent = JSON.stringify(data);
+    })
+
+    p.on('stream', function (stream) {
+        let video = document.querySelector('#receiver-video');
+        video.src = window.URL.createObjectURL(stream);
+        video.play();
+    })
+
+    document.querySelector('#incoming').addEventListener('submit', function (e) {
+        e.preventDefault();
+        p.signal(JSON.parse(e.target.querySelector('textarea').value));
+    });
+
+}
+
+function startPeer(initiator) {
     navigator.getUserMedia({
         video: true,
         audio: true
     }, function (stream) {
+        let p = new SimplePeer({
+            initiator: true,
+            stream: stream,
+            trickle: false
+        })
+        bindEvents(p);
         let emitterVideo = document.querySelector('#emitter-video');
-        emitterVideo = window.URL.createObjectURL(stream);
+        emitterVideo.src = window.URL.createObjectURL(stream);
         emitterVideo.play();
+    }, function () {
+
     })
+}
+
+document.querySelector('#start').addEventListener('click', function (e) {
+    startPeer(true)
 });
+
+document.querySelector('#receive').addEventListener('click', function (e) {
+    startPeer(false)
+});
+
